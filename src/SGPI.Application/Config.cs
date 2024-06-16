a5b911b3a8defd68c952d2ce4726b7fa7dae6d59
Jean Markis <jeanmarkis85@gmail.com>
2024-06-16 15:52:55 -0300

    chore: Reformat and Cleanup

--- src/SGPI.Application/Config.cs
+++ src/SGPI.Application/Config.cs
@@ -5,7 +5,6 @@ public static class Config
     public const string RouteDocs = "docs";
     public const string NameApplication = "SGPI API";
     public const string UrlSwagger = "/swagger/v1/swagger.json";
-    
+
     public const string ConnectionStringAppDb = "AppDb";
-    
 }
